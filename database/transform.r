9b52f6889b40d22a998b9438c66cb572fd3ac3cd
JoaoGabrielPereiraLopes <147850438+JoaoGabrielPereiraLopes@users.noreply.github.com>
2025-01-29 17:42:31 -0300

    Delete database directory

--- database/transform.r
+++
@@ -1,5 +0,0 @@
-library(RSQLite)
-data=read.csv("./death_projection.csv", header = TRUE, sep = ";")
-con <- dbConnect(RSQLite::SQLite(), "DeathsProjection.db")
-
-dbWriteTable(con, "pessoas", data, append = TRUE, row.names = FALSE)
