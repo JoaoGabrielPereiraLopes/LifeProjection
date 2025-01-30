8e6aa27a0733cf8cdb57e1f56b2217a3cc59a751
JoaoGabrielPereiraLopes <147850438+JoaoGabrielPereiraLopes@users.noreply.github.com>
2025-01-30 11:42:14 -0300

    Delete database/query_join.sql

--- database/query_join.sql
+++
@@ -1,6 +0,0 @@
-SELECT Prevision.*, 
-    printf("%.3f", State.HDI) AS StateHDI, 
-    printf("%.2f", State.Income) AS StateIncome
-  FROM Prevision
-  INNER JOIN State ON Prevision.State = State.ID;
-DROP TABLE Prevision;
